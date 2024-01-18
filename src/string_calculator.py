0f04bd74a592037ae218d1c2ebe3354a9b6a98ea
Florian Schaufelberger <florian.schaufelberger@dentagest.ch>
2024-01-18 09:24:05 +0100

    reset

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -12,12 +12,3 @@ class StringCalculator:
                 sum_numbers = 0
         
         return sum_numbers
-    
-    def multiply(string_numbers):
-        if ";" in string_numbers and not(any(c.isalpha() for c in string_numbers)): 
-            numbers = string_numbers.split(";")
-            prod_number = math.prod(numbers)
-        else:
-            prod_number = 0
-
-        return prod_number
